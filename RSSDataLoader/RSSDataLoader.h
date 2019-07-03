f8ebc86fe71d5bfc6120f1a68046c67e0d257d4d
Santhosh <santhosh_nampally@epam.com>
2019-07-03 18:09:35 +0530

    Initial Commit

---
+++ RSSDataLoader/RSSDataLoader.h
@@ -0,0 +1,19 @@
+//
+//  RSSDataLoader.h
+//  RSSDataLoader
+//
+//  Created by Santhosh Nampally on 03/07/19.
+//  Copyright © 2019 Santhosh Nampally. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RSSDataLoader.
+FOUNDATION_EXPORT double RSSDataLoaderVersionNumber;
+
+//! Project version string for RSSDataLoader.
+FOUNDATION_EXPORT const unsigned char RSSDataLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RSSDataLoader/PublicHeader.h>
+
+
